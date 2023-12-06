794e187cac001e0ad500957f449ae21b72d51202
BaloghBalazs91 <blay1991@gmail.com>
2023-12-06 14:41:07 +0100

    Docker frontend created

--- inventory-management-app/src/Components/PrivateRoute.jsx
+++ inventory-management-app/src/Components/PrivateRoute.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const PrivateRoute = ({ element, roles }) => {
     const userRole = JSON.parse(localStorage.getItem('role'));
