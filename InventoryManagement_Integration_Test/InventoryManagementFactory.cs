d8c21075405420dfb7cb3d64cb0df5f1f82147b7
vanda.szabo <msvandaszabo@gmail.com>
2023-12-07 15:19:28 +0100

    Added Integration Test project

---
+++ InventoryManagement_Integration_Test/InventoryManagementFactory.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Inventory_Integration_Test;
+
+public class InventoryManagementFactory : WebApplicationFactory<Program>
+{
+    
+}
